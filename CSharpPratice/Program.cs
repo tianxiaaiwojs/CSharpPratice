a0682b3145ff6f29b4c36e552aeeb13d44d6a426
ZHU BEIBEI <tianxiaaiwojs@gmail.com>
2017-07-21 16:04:51 +0800

    add gitignore file

--- CSharpPratice/Program.cs
+++ CSharpPratice/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SixCharpterArray;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,10 @@ namespace CSharpPratice
     {
         static void Main(string[] args)
         {
+            var createArrayInstance = new CreateArray();
+            createArrayInstance.CreateInstance();
+
+            Console.ReadLine();
         }
     }
 }
---
+++ SixCharpterArray/CreateArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SixCharpterArray
+{
+    public class CreateArray
+    {
+        public void CreateInstance()
+        {
+            Array intArray = Array.CreateInstance(typeof(int), 5);
+            
+
+            int[] intArrayCopy = (int[])intArray;
+            int[] lengths = {2,3};
+            int[] lowerBound = {1,10};
+            var multipleDim = Array.CreateInstance(typeof(int),lengths,lowerBound);
+            multipleDim.SetValue(1, 1, 11);
+            Console.WriteLine(multipleDim.GetValue(1, 11));
+
+
+        }
+    }
+}
